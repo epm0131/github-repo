9dbbb04752cee17b11248c8a6458f58e00a0fa4c
Ettore Merendino <merendino.ettore@gmail.com>
2016-11-18 11:18:58 -0500

    I setup my ajax call to pull a random repo from the API

--- src/js/app.js
+++ src/js/app.js
@@ -2,20 +2,28 @@
   'use strict';
 
   window.git = window.git || {};
+  window.git.searchRepos = searchRepos;
   /**
    * It is supposed to be able to search the github api
    *
    * @return {[type]} [description]
    */
-  function searchQuery(Promise) {
-    $.ajax({
-      url: 'https://api.github.com/search/repositories?q=...',
+  function searchRepos(api, query) {
+    return $.ajax({
+      url: 'https://api.github.com/search/repositories?q=' + query,
       method: 'GET',
       dataType: 'json',
       headers: {
-        Authorization: 'token'
+        Authorization: 'token ' + api
       }
+    })
+    .then(function handleSuccess(data) {
+      var repos = data.items;
+      var randomRepo = repos[Math.floor(Math.random()*repos.length)];
+      console.log(randomRepo);
+
     });
+
   }
 
 
